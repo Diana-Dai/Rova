cb583d6cf426252e0742f7c7e8069167023ae826
Diana <dinamenediana@gmail.com>
2022-08-23 10:38:09 +1000

    updated featured collection

---
+++ assets/custom.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    var elem = document.querySelector('.collection-list__container ul');
+    var flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'left'
+    // contain: true
+    });
+  }
