60d0628e6a87b6ac783b8230a8cf3d1fc80775a2
Diana <dinamenediana@gmail.com>
2022-08-25 15:19:01 +1000

    added the shop the look section

--- assets/custom.js
+++ assets/custom.js
@@ -1,8 +1,24 @@
 window.onload = function () {
-    var elem = document.querySelector('.collection-list__container ul');
-    var flkty = new Flickity( elem, {
-    // options
-    cellAlign: 'left'
-    // contain: true
+
+    $('.collection-list__content').flickity({
+      cellAlign: 'left'
+    });
+
+    // shop the look
+    var shop_the_look_flkty = $('.shoppable_images_carousel').flickity({
+      cellAlign: 'left'
+    });
+
+    $(document).on('click','.shoppable_images_carousel_navigation .navigation',function () {  
+        $('.shoppable_images_carousel_navigation .is-selected').removeClass('is-selected');
+        $(this).addClass('is-selected');
+        var index = $(this).index();
+        shop_the_look_flkty.flickity( 'select', index );
+    })
+
+    shop_the_look_flkty.on( 'change.flickity', function( event, index ) {
+      console.log( 'Slide changed to ' + index )
+      $('.shoppable_images_carousel_navigation .is-selected').removeClass('is-selected');
+      $('.shoppable_images_carousel_navigation [data-index='+index +']').addClass('is-selected')
     });
   }
